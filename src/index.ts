80f89dd7affb8a7d1c486752cf3f0659bcd28715
Nikolas Vahlas <nikolasvahlas@nikolasvahlas.com>
2024-12-11 17:25:58 -0800

    fix duplicate coin problem

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
+import { readFile } from 'fs/promises';
 import { getContractAddress } from './notification_logic/getContractAddress.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { appendToCoinsSeen } from './write_coins_seen.js';
-import * as addressesSeenJson from './contract_addresses_seen.json';
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 interface AddressesSeenJson {
   seenAddresses: string[];
 }
 
 async function main() {
-  const typedAddressesSeenJson: AddressesSeenJson = addressesSeenJson;
+  const filePath = path.resolve(__dirname, './contract_addresses_seen.json');
+  const fileContents = await readFile(filePath, 'utf8');
+  const addressesSeenJson: AddressesSeenJson = JSON.parse(fileContents);
+  const addressesSeen = new Set(addressesSeenJson.seenAddresses);
 
-  const addressesSeen = new Set(typedAddressesSeenJson.seenAddresses);
   const walletAddress = 'ssssss';
   while (true) {
     const currentContractAddress: string | null = await getContractAddress();
